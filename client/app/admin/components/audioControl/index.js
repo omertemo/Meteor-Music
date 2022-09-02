b1e44d7df780697dc8da7bbdde478c3a1631ce20
omertemo <omer_temo@hotmail.com>
2022-09-02 15:52:21 +0300

    play stop sorunu çözülmedi

--- client/app/admin/components/audioControl/index.js
+++ client/app/admin/components/audioControl/index.js
@@ -1,21 +1,21 @@
 Template.adminComponentsAudioControl.onCreated(function () {
+  source = null;
   this.state = new ReactiveDict(null, {
     audioBuf: String,
+    isPlay: Boolean,
+    tempThis: Object,
   });
 });
-Template.adminComponentsAudioControl.helpers({
-  isPlay: function () {
-    if (source) {
-      return true;
-    }
-  },
-});
+
 Template.adminComponentsAudioControl.events({
   "click #startButton": function (event, template) {
+    // AppUtil.temp.set("song", template);
+
+    template.state.set("isPlay", true);
     if (source) {
       source.stop(0);
     }
-    // base64Val = AppUtil.temp.get("audioBufTemp");
+
     if (!this.arrayBuf) {
       audioBuffer = AudioGlobal.audioFunction(base64Val);
     } else {
@@ -28,12 +28,15 @@ Template.adminComponentsAudioControl.events({
       source.buffer = audioBuffer;
       source.loop = false;
       source.connect(context.destination);
+      console.log("Müzik Oynatılıyor");
       source.start(0); // Play immediately.
     }, 1000);
   },
 
   "click #stopButton": function (event, template) {
     if (source) {
+      template.state.set("isPlay", false);
+      console.log("Müzik Durduruldu");
       source.stop(0);
     }
   },
